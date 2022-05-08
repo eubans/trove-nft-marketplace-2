0aa449228f06fe4ad919ea6b8b29d76cc2407fed
Mark Eubans <markeubans@Marks-MacBook-Air.local>
2022-05-09 02:19:18 +0800

    upload media files

--- next.config.js
+++ next.config.js
@@ -7,12 +7,9 @@ module.exports = {
     },
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
         // eslint-disable-next-line no-param-reassign
-        config.ignoreWarnings = [
-            {
-                message:
-                    /(magic-sdk|@walletconnect\/web3-provider|@web3auth\/web3auth)/,
-            },
-        ];
+        config.ignoreWarnings = [{
+            message: /(magic-sdk|@walletconnect\/web3-provider|@web3auth\/web3auth)/,
+        }, ];
         return config;
     },
 };
